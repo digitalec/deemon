cb44523d6d098fb981aab061aaf5d12d402a90e4
Shawn Eggleston <github@gradiian.io>
2021-05-30 22:21:43 -0400

    move logger outside of class

--- deemon/app/notify.py
+++ deemon/app/notify.py
@@ -4,14 +4,14 @@ import logging
 from email.utils import formataddr
 from email.message import EmailMessage
 
+logger = logging.getLogger("deemon")
+
 
 class EmailNotification:
 
-    logger = logging.getLogger("deemon")
-
     def __init__(self, config):
         self.config = config
-        self.enable_notify = True if self.config["smtp_server"]  != "" else False
+        self.enable_notify = True if self.config["smtp_server"] != "" else False
 
     def notify(self, new_releases=[], test=False):
         if test:
@@ -30,11 +30,11 @@ class EmailNotification:
 
             context = ssl.create_default_context()
 
-            self.logger.debug("Sending new release notification email")
-            self.logger.debug(f"Using server: {self.config['smtp_server']}:{self.config['smtp_port']}")
+            logger.debug("Sending new release notification email")
+            logger.debug(f"Using server: {self.config['smtp_server']}:{self.config['smtp_port']}")
             with smtplib.SMTP_SSL(self.config["smtp_server"], self.config["smtp_port"], context=context) as server:
                 try:
                     server.login(self.config["smtp_username"], self.config["smtp_password"])
                     server.sendmail(self.config["smtp_sender_email"], self.config["smtp_recipient"], msg.as_string())
                 except Exception as e:
-                    self.logger.error("Error while sending mail: " + str(e))
+                    logger.error("Error while sending mail: " + str(e))
