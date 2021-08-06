bca04773940f547f625c8f69c18e35f8d9802c6d
digitalec <31838395+digitalec@users.noreply.github.com>
2021-08-06 14:22:15 -0400

    refactored Download module

--- deemon/app/download.py
+++ deemon/app/download.py
@@ -14,9 +14,9 @@ logger = logging.getLogger(__name__)
 
 class QueueItem:
 
-    def __init__(self, artist=None, album=None, playlist=None):  # TODO - Accept new playlist tracks for output/alerts
+    def __init__(self, bitrate, artist=None, album=None, playlist=None):  # TODO - Accept new playlist tracks for output/alerts
         self.artist_name = None
-        self.bitrate = None
+        self.bitrate = bitrate
         self.album_id = None
         self.album_title = None
         self.url = None
@@ -25,20 +25,17 @@ class QueueItem:
 
         if artist:
             self.artist_name = artist["name"]
-            self.bitrate = artist["bitrate"]
             self.url = artist["link"]
 
         if album:
             if not artist:
                 self.artist_name = album["artist"]["name"]
-                self.bitrate = album["bitrate"]
             self.album_id = album["id"]
             self.album_title = album["title"]
             self.url = album["link"]
 
         if playlist:
-            self.bitrate = artist["bitrate"]
-            self.url = playlist["url"]
+            self.url = playlist["url"]  # TODO - need consistency, should be LINK
             self.playlist_title = playlist["title"]
 
         self.print_queue_to_log()
@@ -88,19 +85,19 @@ class Download:
             logger.error("Error: Plex library not found. See logs for additional info.")
             logger.debug(f"Error during Plex refresh: {e}")
 
-    def download_queue(self, queue):  # TODO - Add support for downloading just Artist
-        if queue:
+    def download_queue(self):
+        if self.queue_list:
             plex = self.get_plex_server()
             print("----------------------------")
-            logger.info("Sending " + str(len(queue)) + " release(s) to deemix for download:")
+            logger.info("Sending " + str(len(self.queue_list)) + " release(s) to deemix for download:")
 
-            for q in queue:
+            for q in self.queue_list:
                 if self.verbose == "true":
                     logger.debug(f"Processing queue item {vars(q)}")
                 if q.artist_name:
                     logger.info(f"+ {q.artist_name} - {q.album_title}... ")
                 else:
-                    logger.info(f"+ Updating playlist: {q.playlist_title}...")
+                    logger.info(f"+ {q.playlist_title} (playlist)...")
                 self.di.download_url([q.url], q.bitrate)
 
             print("")
@@ -108,18 +105,7 @@ class Download:
             if plex and (self.config["plex_library"] != ""):  # TODO - config validation should be done elsewhere
                 self.refresh_plex(plex)
 
-    # TODO Re-write manual download option - add_to_queue only used by download()
-    def add_to_queue(self, artist, album):
-        for _album in album['data']:
-            if (self.record_type == _album["record_type"]) or (self.record_type == "all"):
-                logger.debug(f"QUEUE: Adding '{_album['title']}' to queue...")
-                artist["bitrate"] = self.bitrate
-                self.queue_list.append(QueueItem(artist, _album))
-
-    # TODO Re-write manual download option
-    def download(self, artist, artist_id, album_id, url, bitrate, record_type, input_file):
-
-        # TODO Download via URL needs to be reworked / self.di.download_url()
+    def download(self, artist, artist_id, album_id, url, bitrate, record_type, input_file, auto=True):
 
         def filter_artist_by_record_type(artist, record_type):
             album_api = self.dz.api.get_artist_albums(artist['id'])['data']
@@ -127,9 +113,12 @@ class Download:
             for album in album_api:
                 if (album['record_type'] == record_type) or record_type == "all":
                     filtered_albums.append(album)
+                else:
+                    logger.debug(f"Filtered release '{album['title']} ({album['id']})' as it "
+                                 f"does not match record type '{record_type}'")
             return filtered_albums
 
-        def get_api_result(artist=None, artist_id=None, album_id=None):  # TODO Multiple try for detailed logging
+        def get_api_result(artist=None, artist_id=None, album_id=None):
             if artist:
                 try:
                     return self.dz.api.search_artist(artist, limit=1)["data"][0]  # TODO handle incorrect artist
@@ -147,11 +136,10 @@ class Download:
                     logger.error(f"Album ID {album_id} not found.")
 
         def queue_filtered_releases(api_object):
-            api_object['bitrate'] = bitrate
             filtered = filter_artist_by_record_type(api_object, record_type)
             for album in filtered:
                 if not queue_item_exists(album['id']):
-                    self.queue_list.append(QueueItem(api_object, album))
+                    self.queue_list.append(QueueItem(bitrate, api_object, album))
 
         def queue_item_exists(i):
             for q in self.queue_list:
@@ -168,7 +156,7 @@ class Download:
                 sorted_list = sorted(list(filter(None, csv_to_list)))
                 return sorted_list
 
-        def check_for_artist_ids(artist_list):
+        def process_input_file(artist_list):
             logger.debug("Processing file contents")
             int_artists = []
             str_artists = []
@@ -180,70 +168,84 @@ class Download:
             logger.debug(f"Detected {len(int_artists)} artist ID(s) and {len(str_artists)} artist name(s)")
             return int_artists, str_artists
 
-        def download_by_name(artist):
-            for a in artist:
-                artist_result = get_api_result(artist=a)
-                if artist_result:
-                    queue_filtered_releases(artist_result)
+        def process_artist_by_name(name):
+            logger.debug("Processing artists by name")
+            artist_result = get_api_result(artist=name)
+            logger.debug(f"Requested Artist: '{name}', Found: '{artist_result['name']}'")
+            if artist_result:
+                queue_filtered_releases(artist_result)
 
-        def download_by_id(artist_id):
-            for i in artist_id:
-                artist_id_result = get_api_result(artist_id=i)
-                if artist_id_result:
-                    queue_filtered_releases(artist_id_result)
+        def process_artist_by_id(id):
+            logger.debug("Processing artists by ID")
+            artist_id_result = get_api_result(artist_id=id)
+            logger.debug(f"Requested Artist ID: {id}, Found: {artist_id_result['name']}")
+            if artist_id_result:
+                queue_filtered_releases(artist_id_result)
+
+        def process_album_by_id(id):
+            logger.debug("Processing album by name")
+            album_id_result = get_api_result(album_id=id)
+            logger.debug(f"Requested Album: {id}, "
+                         f"Found: {album_id_result['artist']['name']} - {album_id_result['title']}")
+            if album_id_result and not queue_item_exists(album_id_result['id']):
+                self.queue_list.append(QueueItem(bitrate, album=album_id_result))
+
+        def process_playlist_by_id(id):
+            playlist_api = self.dz.api.get_playlist(id)
+            # Add new entries that QueueItem is looking for
+            playlist_api['url'] = playlist_api['link']
+            self.queue_list.append(QueueItem(bitrate, playlist=playlist_api))
+
+        def extract_id_from_url(url):
+            id_group = ['artist', 'album', 'playlist']
+            for group in id_group:
+                id_type = group
+                try:
+                    id = int(url.split(f'/{group}/')[1])
+                    logger.debug(f"Extracted group={id_type}, id={id}")
+                    return id_type, id
+                except (IndexError, ValueError) as e:
+                    continue
+            return False, False
 
         if artist:
-            logger.debug("Downloading by Artist")
-            download_by_name(artist)
+            [process_artist_by_name(a) for a in artist]
 
         if artist_id:
-            logger.debug("Downloading by Artist ID")
-            download_by_id(artist_id)
+            [process_artist_by_id(i) for i in artist_id]
 
         if album_id:
-            logger.debug("Downloading by Album ID")
-            logger.info(album_id)
-            for i in album_id:
-                record_type = "all"
-                album_id_result = get_api_result(album_id=i)
-                if album_id_result and not queue_item_exists(album_id_result['id']):
-                    album_id_result['bitrate'] = bitrate
-                    self.queue_list.append(QueueItem(album=album_id_result))
+            [process_album_by_id(i) for i in album_id]
 
         if input_file:
             logger.info(f"Reading from file {input_file}")
             if Path(input_file).exists():
                 artist_list = read_file_as_csv(input_file)
-                artist_int_list, artist_str_list = check_for_artist_ids(artist_list)
+                artist_int_list, artist_str_list = process_input_file(artist_list)
                 if artist_str_list:
-                    download_by_name(artist_str_list)
+                    for artist in artist_str_list:
+                        process_artist_by_name(artist)
                 if artist_int_list:
-                    download_by_id(artist_int_list)
+                    for artist in artist_int_list:
+                        process_artist_by_id(artist)
 
         if url:
+            logger.debug("Processing URLs")
             for u in url:
-                id_type = ""
-                id = 0
-                try:
-                    id_type = "artist"
-                    id = u.split('/artist/')
-                except Exception as e:
-                    logger.error(e)
+                egroup, eid = extract_id_from_url(u)
+                if not egroup or not eid:
+                    logger.error(f"Invalid URL -- {u}")
+                    continue
 
-                try:
-                    id_type = "album"
-                    id = u.split('/album/')
-                except Exception as e:
-                    logger.error(e)
-
-                try:
-                    id_type = "playlist"
-                    id = u.split('/playlist/')
-                except Exception as e:
-                    logger.error(e)
-
-            logger.info(f"id_type={id_type} ---- id={id}")
+                if egroup == "artist":
+                    process_artist_by_id(eid)
+                elif egroup == "album":
+                    process_album_by_id(eid)
+                elif egroup == "playlist":
+                    process_playlist_by_id(eid)
 
         if self.duplicate_id_count > 0:
             logger.info(f"Cleaned up {self.duplicate_id_count} duplicate release(s). See log for additional info.")
-        self.download_queue(self.queue_list)
+
+        if auto:
+            self.download_queue()
