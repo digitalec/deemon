29dca5d3057fbbcf9ee97185ddfdf71566b43294
digitalec <31838395+digitalec@users.noreply.github.com>
2021-09-11 02:13:00 -0400

    Added back deemix log capturing

--- deemon/core/logger.py
+++ deemon/core/logger.py
@@ -28,6 +28,9 @@ def setup_logger(log_level='DEBUG', log_file=None):
     deemon_logger = logging.getLogger("deemon")
     deemon_logger.setLevel(logging.DEBUG)
 
+    deemix_logger = logging.getLogger("deemix")
+    deemix_logger.setLevel(logging.DEBUG)
+
     urllib3_logger = logging.getLogger("urllib3")
     urllib3_logger.setLevel(logging.INFO)
 
@@ -35,6 +38,7 @@ def setup_logger(log_level='DEBUG', log_file=None):
     spotipy_logger.setLevel(logging.INFO)
 
     del logger.handlers[:]
+    del deemix_logger.handlers[:]
 
     if log_file is not None:
         rotate = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=5, encoding="utf-8")
