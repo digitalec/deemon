96add28d1ed0ae4ab0fd377e3ed5de1e20284aae
digitalec <31838395+digitalec@users.noreply.github.com>
2021-09-14 16:54:39 -0400

    Fixes issue where comma in artist name treated as delimiter

--- deemon/utils/dataprocessor.py
+++ deemon/utils/dataprocessor.py
@@ -1,12 +1,15 @@
+import csv
 import logging
+from csv import reader
+import re
 
 logger = logging.getLogger(__name__)
 
 
 def read_file_as_csv(file):
     with open(file, 'r', encoding="utf8", errors="replace") as f:
-        make_csv = f.read().replace('\n', ',')
-        csv_to_list = make_csv.split(',')
+        make_csv = f.read()
+        csv_to_list = make_csv.split('\n')
         sorted_list = sorted(list(filter(None, csv_to_list)))
         return sorted_list
 
@@ -24,8 +27,23 @@ def process_input_file(artist_list):
     return int_artists, str_artists
 
 
-def artists_to_csv(a):
-    csv_artists = ' '.join(a)
-    csv_artists = csv_artists.split(',')
-    csv_artists = [x.lstrip() for x in csv_artists]
-    return csv_artists
+def artists_to_csv(all_artists) -> list:
+    """
+    Separate artists and replace delimiter to find artists containing commas in their name
+    """
+    all_artists = [x for x in all_artists]
+    processed_artists = []
+    for artist in all_artists:
+        if artist[-1] == ',':
+            processed_artists.append(artist[:-1] + "|")
+        else:
+            processed_artists.append(artist)
+    processed_artists = ' '.join(processed_artists)
+    processed_artists = processed_artists.split('|')
+
+    result = []
+    csv_artists = reader(processed_artists, delimiter="|")
+    for line in csv_artists:
+        combined_line = ([x.lstrip() for x in line])
+        result.append(','.join(combined_line))
+    return(result)
