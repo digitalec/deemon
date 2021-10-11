cada911172d73bc3ffeb352ab0d02c59d9d1dd72
digitalec <31838395+digitalec@users.noreply.github.com>
2021-10-11 19:55:17 -0400

    Display backups newest to oldest

--- deemon/cmd/backup.py
+++ deemon/cmd/backup.py
@@ -118,7 +118,7 @@ def restore():
         if tar_files:
             backups.append(tar_files)
     if backups:
-        backups = sorted(backups, key=lambda x: x['filename'], reverse=False)
+        backups = sorted(backups, key=lambda x: x['filename'], reverse=True)
         display_backup_list(backups)
     else:
         logger.info("No backups available to restore")
