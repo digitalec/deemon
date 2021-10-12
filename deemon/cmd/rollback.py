c8fb4439e9d8475ba20b04c74b8a16ab8a6a431c
digitalec <31838395+digitalec@users.noreply.github.com>
2021-10-11 21:34:38 -0400

    Fix KeyError when selecting transaction

--- deemon/cmd/rollback.py
+++ deemon/cmd/rollback.py
@@ -8,28 +8,28 @@ db = Database()
 
 
 def view_transactions():
-    tr = db.get_transactions()
-    if not tr:
+    transactions = db.get_transactions()
+    if not transactions:
         return logger.info("No transactions are available to be rolled back.")
 
-    for i, ld in enumerate(tr, start=1):
+    for i, transaction in enumerate(transactions, start=1):
         release_id = []
         artist_names = []
         playlist_titles = []
 
-        for k, v in ld.items():
-            if (k == "releases" or k == "playlist_tracks") and ld[k]:
-                for item in ld[k]:
+        for k, v in transaction.items():
+            if (k == "releases" or k == "playlist_tracks") and transaction[k]:
+                for item in transaction[k]:
                     for key, val in item.items():
                         if key == "album_id" or key == "track_id":
                             if item[key] not in release_id:
                                 release_id.append(item[key])
-            if k == "monitor" and ld[k]:
-                if ld[k] not in artist_names:
-                    artist_names = [x['artist_name'] for x in ld[k]]
-            if k == "playlists" and ld[k]:
-                if ld[k] not in playlist_titles:
-                    playlist_titles.append(ld[k][0]['title'])
+            if k == "monitor" and transaction[k]:
+                if transaction[k] not in artist_names:
+                    artist_names = [x['artist_name'] for x in transaction[k]]
+            if k == "playlists" and transaction[k]:
+                if transaction[k] not in playlist_titles:
+                    playlist_titles.append(transaction[k][0]['title'])
 
         release_id_len = len(release_id)
 
@@ -64,10 +64,10 @@ def view_transactions():
             else:
                 output_text = f"Added {release_text[4:]}"
 
-        print(f"{i}. {dates.get_friendly_date(ld['timestamp'])} - {output_text}")
+        print(f"{i}. {dates.get_friendly_date(transaction['timestamp'])} - {output_text}")
 
     rollback = None
-    while rollback not in range(len(tr)):
+    while rollback not in range(len(transactions)):
         rollback = input("\nSelect specific refresh to rollback (or press Enter to exit): ")
         if rollback == "":
             return
@@ -76,7 +76,7 @@ def view_transactions():
         except ValueError:
             logger.error("Invalid input")
 
-    rollback = tr[rollback][0]['id']
+    rollback = transactions[rollback]['id']
     logger.debug(f"Rolling back transaction {rollback}")
     db.rollback_refresh(rollback)
 
