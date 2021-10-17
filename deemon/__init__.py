ecdf36c205c1d3ef4d8f44186917cdfee07ff627
digitalec <31838395+digitalec@users.noreply.github.com>
2021-10-17 12:42:53 -0400

    Fix version to allow db upgrade to 3.3

--- deemon/__init__.py
+++ deemon/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from deemon.utils import startup
 
-__version__ = '2.4.1'
-__dbversion__ = '3.2'
+__version__ = '2.4.2'
+__dbversion__ = '3.3'
 
 appdata = startup.get_appdata_dir()
 startup.init_appdata_dir(appdata)
