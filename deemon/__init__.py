5cab1e39e6a66029d5a14ac8f88a8d75200c7328
digitalec <31838395+digitalec@users.noreply.github.com>
2021-11-13 00:52:12 -0500

    bump version to 2.6.1

--- deemon/__init__.py
+++ deemon/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from deemon.utils import startup
 
-__version__ = '2.6.0'
-__dbversion__ = '3.5.1'
+__version__ = '2.6.1'
+__dbversion__ = '3.5.2'
 
 appdata = startup.get_appdata_dir()
 startup.init_appdata_dir(appdata)
