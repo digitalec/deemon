5f22351a2ccbd29caca8b29f445465c904ba7011
digitalec <31838395+digitalec@users.noreply.github.com>
2021-11-14 22:18:34 -0500

    bump version to 2.8.0-beta1

--- deemon/__init__.py
+++ deemon/__init__.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from deemon.utils import startup
 
-__version__ = '2.7.0'
-__dbversion__ = '3.5.2'
+__version__ = '2.8.0-beta1'
+__dbversion__ = '3.6'
 
 appdata = startup.get_appdata_dir()
 startup.init_appdata_dir(appdata)
