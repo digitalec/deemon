648f73529b04dd149d3f9455a558b43f4ddbb011
digitalec <31838395+digitalec@users.noreply.github.com>
2022-03-17 13:19:58 -0400

    separated stream and rotate log formats

--- deemon/core/logger.py
+++ deemon/core/logger.py
@@ -5,13 +5,18 @@ import tqdm
 
 LOG_FORMATS = {
     'DEBUG': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+    'INFO': '%(asctime)s [%(levelname)s] %(name)s: %(message)s',
+}
+
+STREAM_LOG_FORMATS = {
+    'DEBUG': '%(message)s',
     'INFO': '%(message)s',
 }
 
 LOG_DATE = '%Y-%m-%d %H:%M:%S'
 
 
-class tqdmStream(object):
+class TqdmStream(object):
 
     @classmethod
     def write(cls, msg):
@@ -26,20 +31,21 @@ def setup_logger(log_level='DEBUG', log_file=None):
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    deemon_logger = logging.getLogger("deemon")
+    deemon_logger = logging.getLogger("mercury")
     deemon_logger.setLevel(logging.DEBUG)
 
-    deemix_logger = logging.getLogger("deemix")
-    deemix_logger.setLevel(logging.DEBUG)
-
-    urllib3_logger = logging.getLogger("urllib3")
-    urllib3_logger.setLevel(logging.ERROR)
-
-    spotipy_logger = logging.getLogger("spotipy")
-    spotipy_logger.setLevel(logging.INFO)
+    # TODO REMOVE
+    # deemix_logger = logging.getLogger("deemix")
+    # deemix_logger.setLevel(logging.DEBUG)
+    #
+    # urllib3_logger = logging.getLogger("urllib3")
+    # urllib3_logger.setLevel(logging.ERROR)
+    #
+    # spotipy_logger = logging.getLogger("spotipy")
+    # spotipy_logger.setLevel(logging.INFO)
 
     del logger.handlers[:]
-    del deemix_logger.handlers[:]
+    # del deemix_logger.handlers[:]
 
     if log_file is not None:
         rotate = RotatingFileHandler(log_file, maxBytes=2097152, backupCount=5, encoding="utf-8")
@@ -47,7 +53,7 @@ def setup_logger(log_level='DEBUG', log_file=None):
         rotate.setFormatter(logging.Formatter(LOG_FORMATS['DEBUG'], datefmt=LOG_DATE))
         logger.addHandler(rotate)
 
-    stream = logging.StreamHandler(stream=tqdmStream)
+    stream = logging.StreamHandler(stream=TqdmStream)
     stream.setLevel(log_level)
-    stream.setFormatter(logging.Formatter(LOG_FORMATS[log_level], datefmt=LOG_DATE))
+    stream.setFormatter(logging.Formatter(STREAM_LOG_FORMATS[log_level], datefmt=LOG_DATE))
     deemon_logger.addHandler(stream)
