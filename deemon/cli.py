a7e22252a0c65804083518a7cedf9d6c86defcbd
digitalec <31838395+digitalec@users.noreply.github.com>
2022-03-24 18:29:01 -0400

    update save location of args

--- deemon/cli.py
+++ deemon/cli.py
@@ -2,7 +2,8 @@ import logging
 import requests
 
 from deemon import VERSION
-from deemon.cmd import monitor
+from deemon.cmd import monitor, refresh
+from deemon.core import notifier
 from deemon.core.db import Database
 from deemon.core.config import Config
 from deemon.core.exceptions import InvalidValue
@@ -45,33 +46,31 @@ def cli(args):
             if args.include_logs:
                 pass
         pass
-    elif args.command == 'config':
-        pass
     elif args.command == 'download':
         pass
     elif args.command == 'monitor':
         if args.alerts:
-            config['alerts']['enabled'] = True
+            config['runtime']['alerts'] = True
         if args.bitrate:
             if validate.validate_bitrates(args.bitrate):
-                config['defaults']['bitrate'] = args.bitrate
+                config['runtime']['bitrate'] = args.bitrate
             else:
                 raise InvalidValue(f"Invalid bitrate: {args.bitrate}")
         if args.download_path:
-            config['defaults']['download_path'] = args.download_path
+            config['runtime']['download_path'] = args.download_path
         if args.record_type:
             invalid_rt = validate.validate_record_type(args.record_type)
             if len(invalid_rt):
                 raise InvalidValue(f"Invalid record type(s): {args.record_type}")
             else:
-                config['defaults']['record_type'] = recordtypes.get_record_type_index(args.record_type)
+                config['runtime']['record_type'] = recordtypes.get_record_type_index(args.record_type)
         if args.artist:
             config['runtime']['artist'] = dataprocessor.csv_to_list(args.artist)
         if args.artist_id:
-            ids = [x.replace(',','') for x in args.artist_id]
+            ids = [x.replace(',', '') for x in args.artist_id]
             [config['runtime']['artist_id'].append(i) for i in ids if i not in config['runtime']['artist_id']]
         if args.download:
-            config['runtime']['download'] = args.download
+            config['runtime']['download_all'] = args.download
         if args.file:
             config['runtime']['file'] = args.file
         if args.url:
